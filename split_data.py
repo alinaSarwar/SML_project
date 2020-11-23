7b2d4423f00441a0a81705e690dfdb6d33efc222
alinaSarwar <19100036@lums.edu.pk>
2020-11-23 16:57:54 +0500

    read ornament class

--- split_data.py
+++ split_data.py
@@ -5,7 +5,7 @@ import os
 import numpy as np
 
 
-def save_csv(data, path, fieldnames=['image_path', 'pose', 'expressions', 'gender']):
+def save_csv(data, path, fieldnames=['image_path', 'pose', 'expressions', 'gender', 'occlusion']):
     with open(path, 'w', newline='') as csv_file:
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         writer.writeheader()
@@ -43,7 +43,7 @@ if __name__ == '__main__':
             
             if txt_file == []: # some movies have missing txt files
                 continue
-
+            
             fob = open(os.path.join(movie_path, txt_file[0]), 'r').readlines()
             
             for img in fob:
@@ -61,7 +61,7 @@ if __name__ == '__main__':
                     if len(jpgs) != 0: # some annotations have 'null' image names
 
                         img = [j for j in jpgs if search_string in j]
-
+                        
                         if len(img) != 1: # mismatching filenames in annotation and actual image file - Soundarya
                             continue
 
@@ -70,9 +70,13 @@ if __name__ == '__main__':
                             makeup, pose, age, illumination, occlusion, expr, gender = annotation[-1], annotation[-2], annotation[-3], annotation[-4], annotation[-5], annotation[-6], annotation[-7]
                             
                             img_path = os.path.join(movie_path, 'images', img[0])
-                            all_data.append([img_path, pose, expr, gender])
+                            
+                            if occlusion not in ['GLASSES', 'BEARD', 'ORNAMENTS', 'HAIR', 'HAND', 'NONE', 'OTHERS']: # some annotations have missing ornament class
+                                print(movie_path, img)
+                                continue
+                            all_data.append([img_path, pose, expr, gender, occlusion])
 
-    print(len(all_data))
+        print(len(all_data))
 
     # set the seed of the random numbers generator, so we can reproduce the results later
     np.random.seed(42)
@@ -80,10 +84,8 @@ if __name__ == '__main__':
     # construct a Numpy array from the list
     all_data = np.asarray(all_data)
     # split the data into train/val and save them as csv files
-    train_data = all_data[:24177,:]
-    val_data = all_data[24177:,:]
-    # np.random.shuffle(train_data)
-    # np.random.shuffle(val_data)
+    train_data = all_data[:24169,:]
+    val_data = all_data[24169:,:]
 
     
     save_csv(train_data, os.path.join(output_folder, 'train.csv'))
